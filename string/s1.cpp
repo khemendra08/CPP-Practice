0ccdd384eaea4ec9f9776a2bf09b2dd4c3f9a415
khemendra08 <121980206+khemendra08@users.noreply.github.com>
2024-09-18 03:13:19 +0530

    S1 Completed

--- string/s1.cpp
+++ string/s1.cpp
@@ -1,1 +1,10 @@
-//
+//Add new content to the string 
+#include<iostream>
+#include<String>
+using namespace std;
+int main(){
+    string str = "Hello";
+    str.append("World");
+    cout<<str<<endl;
+    return 0;
+}
